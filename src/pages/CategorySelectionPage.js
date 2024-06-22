423d1e2e7555f03e304ead30ad187666d54efbf2
james-aung <james.aung@c-openai.com>
2024-06-22 20:43:00 +0100

    max width of category options

--- src/pages/CategorySelectionPage.js
+++ src/pages/CategorySelectionPage.js
@@ -15,11 +15,13 @@ function CategorySelectionPage(data) {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <h1 className='text-2xl font-bold mb-4 text-center'>Select the categories of policies you want to see:</h1>
-      <CategoryCheckboxes 
-        categories={categories} 
-        selectedCategories={selectedCategories} 
-        handleCategoryChange={handleCategoryChange} 
-      />
+      <div className='w-full max-w-md'>
+        <CategoryCheckboxes 
+          categories={categories} 
+          selectedCategories={selectedCategories} 
+          handleCategoryChange={handleCategoryChange} 
+        />
+      </div>
       <button 
         onClick={handleContinue} 
         className='mt-4 px-4 py-2 border-2 border-blue-500 text-blue-500 rounded-full hover:bg-blue-500 hover:text-white'
